44bea8fee7464618ff1676f6fc7849d069481e35
linuxluola123 <linuxluola123@github.com>
2025-03-04 18:42:41 +0200

    adding some smartness

--- testing_really_unstable/trump.c
+++ testing_really_unstable/trump.c
@@ -9,7 +9,7 @@ int main() {
       "Istut taas valkoisessa talossa ja vaihtoehdot ovat seuraavat \n 1 "
       "hyökkää tanskaan (gröönlanti) \n 2 eroa natosta \n 3 nosta tullimaksuja "
       "\n 4 Heitä ukraina bussin alle ja liittoudu venäjän kanssa \n 0 älä "
-      "tee mitään \n");
+      "tee mitään \n 10 Eroa virastasi \n");
   scanf("%d", &vastaus);
   if (vastaus == 1) {
     printf(
@@ -25,15 +25,18 @@ int main() {
            "tuhoutuu. Kansa syöstää sinut vallasta \n HÄVISIT \n");
   }
   if (vastaus == 4) {
-    printf ("Aloitit kolmannen maailmansodan! Onnittelut! Sinut tullaan muistamaan historiassa! \n");
-
-
-
-
+    printf("Aloitit kolmannen maailmansodan! Onnittelut! Sinut tullaan "
+           "muistamaan historiassa! \n");
   }
   if (vastaus == 0) {
     printf("Pressakautesi päättyy ja valtiota tulee kai viimeinkin johtamaan "
            "jokin järkevä. Voitit.\n ");
+  }
+  if (vastaus == 10) {
+
+    printf("Olet toimillasi saattanut yhdysvallat niin huonoon jamaan että "
+           "päätät erota ja antaa duunin jollekkin pätevälle \nKAIKKI "
+           "VOITTI! \n");
   }
   if (vastaus == 314) {
     printf("opettele kirjoittamaan \n");
